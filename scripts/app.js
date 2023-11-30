11b24f65241c4db8d2e45037df03226f0b8a5716
et120 <e.trotter120@gmail.com>
2023-11-30 14:18:56 -0800

    lecture complete

--- scripts/app.js
+++ scripts/app.js
@@ -7,22 +7,59 @@ let studentEmail = document.getElementById("studentEmail");
 
 
 let students;
+let matchingStudent;
 
 // Create a function that gets our students
 // Create a function that organizes data
 // Event that displays data
 
-function getStudent(){
-    fetch('../data/studentList.json')
-    .then(
-        promise => promise.json()
-    )
-    .then(
-        data => {
-            students = data.StudentList;
-            return students;
-        }
-    )
+
+// function getStudent(){
+//     fetch('../data/studentList.json')
+//     .then(
+//         promise => promise.json()
+//     )
+//     .then(
+//         data => {
+//             students = data.studentList;
+//             console.log(students);
+//             return students;
+//         }
+//     )
+// }
+
+async function getStudent(){
+    const promise = await fetch('../data/studentList.json');
+
+    const data = await promise.json();
+
+    students = data.studentList;
+
+    return students;
 }
+
+function findStudent(){
+    let studentInput = userInput.value.toLowerCase();
+
+    for(let i = 0; i < students.length; i++){
+        let currentStudent = students[i]
+
+        if(currentStudent.firstName.toLowerCase() === studentInput){
+            matchingStudent = currentStudent
+        }
+    }
+
+    if(matchingStudent){
+        studentName.innerText = matchingStudent.firstName + " " + matchingStudent.lastName;
+        studentNumber.innerText = matchingStudent.phoneNumber;
+        studentEmail.innerText = matchingStudent.email;
+    }else{
+        studentName.innerText = "Student not found";
+    }
+}
+
+submitBtn.addEventListener(('click'), function(e){
+    findStudent();
+})
 
 getStudent();
